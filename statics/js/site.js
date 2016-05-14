e86ea8f809ae14aa195d86aaa6d0c574953be001
tzq668766 <tzq618766@gmail.com>
2016-05-15 00:28:50 +0800

    仿写某个网站

--- statics/js/site.js
+++ statics/js/site.js
@@ -1,3 +1,18 @@
 /**
  * Created by Administrator on 15-3-13.
  */
+$(function () {
+    //延迟加载图片
+    $("img").unveil();
+    //回到顶部
+    $.scrollUp({
+        animation: 'fade',
+        activeOverlay: '#00FFFF',
+        scrollImg: {
+            active: false,
+            activeOverlay: false,
+            type: 'background',
+            src: 'statics/images/top.png'
+        }
+    });
+});
